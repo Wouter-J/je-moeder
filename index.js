8112c6236ebedfe2f7b7ea82414884b79a9c37df
survivorbat <mheijden15@avans.nl>
2018-02-28 17:57:43 +0100

    Improved javascript

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var jokes = [
+const jokes = [
         "Je moeder is zo dik dat ze in haar eentje al groepskorting krijgt", 
         "Je moeder is zo lelijk dat ze niet eens kans maakt bij blinden", 
         "Je moeder kan zo slecht schilderen, dat haar schilderijen zichzelf ophangen",
         "Het gewicht van jouw moeder is zo groot dat ze een integer overflow veroorzaakt"
-    ];
+    ]
 
 exports.Joke = function() {
-    var rand = jokes[Math.floor(Math.random() * jokes.length)];
-    console.log(rand);
+    const rand = jokes[Math.floor(Math.random() * jokes.length)]
+    console.log(rand)
 }
 
 exports.JokeString = function() {
-    var rand = jokes[Math.floor(Math.random() * jokes.length)];
-    return rand;
+    const rand = jokes[Math.floor(Math.random() * jokes.length)]
+    return rand
 }
